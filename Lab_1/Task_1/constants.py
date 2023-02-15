b09d7595a2c43a0a2901da8126b64fa8115f1e18
Mikhail Shkarubski <michaelshkarubski@gmail.com>
2023-02-15 23:41:09 +0300

    Added constants.py file for storing latin alphabet, numbers, punctuation marks, etc.

---
+++ Lab_1/Task_1/constants.py
@@ -0,0 +1,6 @@
+import string
+
+LATIN_ALPHABET = tuple(string.ascii_letters)
+NUMBERS = tuple(map(str, range(0, 10)))
+TERMINATION_MARKS = ('...', '.', '!', '?')
+PUNCTUATION_MARKS = (',', ';', '–')
