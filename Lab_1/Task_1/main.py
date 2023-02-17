cfb0605b4a6baba050e53b68819c6e20429d2bf2
Mikhail Shkarubski <michaelshkarubski@gmail.com>
2023-02-17 23:04:25 +0300

    Created a function inside of main function that returns the result of a mathematical operation performed on two numbers. Added docstring to main function. Added type aliases.

--- Lab_1/Task_1/main.py
+++ Lab_1/Task_1/main.py
@@ -1,5 +1,27 @@
+from typing import Union, Literal, Optional
+
+
+# Type aliases for math function
+Operation = Literal["add", "sub", "mult", "div"]
+Number = Union[int, float, complex]
+
+
 def main():
+    """
+    Prints "Hello, World!".
+    Creates a function that returns the result of a mathematical operation.
+    Creates list of numbers and returns list of even numbers in this list.
+    """
     print("Hello, World!")
+
+    def mathfunc(x: Number, y: Number, func: Operation) -> Optional[Number]:
+        """Performs math operations on two numbers."""
+        try:
+            result = eval(f"x.__{func[:3] if func != 'div' else 'truediv'}__(y)")
+        except (AttributeError, TypeError):
+            result = None
+
+        return result
 
 
 if __name__ == "__main__":
